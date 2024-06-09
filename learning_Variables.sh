fc3b7ddca5fae105b1387e8b8d3b0e3bc3c1a281
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-09 20:03:49 +0530

    learned about the variables in bash

---
+++ learning_Variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+arara="ram is ram"
+files=$(ls)
+now=$(date) 
+echo $arara
+
+age="40"
+
+echo "the ssytem time and date is $now "
+
+echo "the content of current directory is $files"
+echo "I'm $age years old "
+echo "$age is tyring "
+echo "$age I am energetic"
