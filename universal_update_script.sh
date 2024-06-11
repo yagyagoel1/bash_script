b97298edf585e40981e8c5ffc9409f5fc573080a
yagyagoel1 <yagyagoel87@gmail.com>
2024-06-11 07:06:09 +0530

    made some changes with the universal update script

--- universal_update_script.sh
+++ universal_update_script.sh
@@ -2,16 +2,17 @@
 
 release_file=/etc/os-release
 
-if gerp -q "Arch" $release_file  
-then 
+if grep -q "Arch" $release_file
+then
  # the host is based on arch ,run the pacman update command
 sudo pacman -Syu
 fi
 
-if grep -q "Ubuntu" $release_file || grep -q "debian" $release_file  
+if grep -q "Ubuntu" $release_file || grep -q "debian" $release_file
 then
-# the host is based on debian or ubuntu 
-# run teh apt version of the command 
+# the host is based on debian or ubuntu
+# run teh apt version of the command
 sudo apt update
 sudo apt dist-upgrade
 fi
+~   
